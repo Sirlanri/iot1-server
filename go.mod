58d06a045b81fa9ade0fbcaa485e47ecb93a89e3
深蓝 <mail@ri-co.cn>
2021-04-09 08:28:42 +0800

    更新依赖

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/Joker/hpp v1.0.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/iris-contrib/middleware/cors v0.0.0-20210110101738-6d0a4d799b5d
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/kataras/iris/v12 v12.2.0-alpha2
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris/v12"
 	"github.com/sirlanri/iot1-server/configs"
+	"github.com/sirlanri/iot1-server/handlers"
 )
 
 func main() {
@@ -20,23 +21,12 @@ func main() {
 	//前端获取实时数据
 	front.Get("/getTemp", handlers.GetTemp)
 	front.Get("/getHumi", handlers.GetHumi)
-	front.Get("/getLight", handlers.GetLight)
-	//front.Get("/getVoice", handlers.GetVoiceTest)
-	front.Get("/getWeek", handlers.GetWeekData)
-	front.Get("/getTimePer", handlers.GetTimePer)
-	front.Get("/getWeekTempHumi", handlers.GetWeekTempHumi)
 
 	//接收传感器发来的内容
 	sensor := m2m.Party("/sensor")
-	sensor.Post("/temp", handlers.SendTemp)
-	sensor.Post("/light", handlers.SendLight)
-	sensor.Post("/humi", handlers.SendHumi)
-	sensor.Post("/body", handlers.SendBody)
+	sensor.Post("/humitemp", handlers.SendHumiTemp)
 
 	//向传感器发送数据
-	sensor.Get("/lighton", handlers.Lighton)
-	sensor.Get("/lightoff", handlers.Lightoff)
-	sensor.Get("/buzzon", handlers.Buzzon)
 
 	app.Run(iris.Addr(configs.PortConfig()))
 
