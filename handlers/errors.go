400ce730f4fbdb12c7f28f623eb2c5b6cfb29754
深蓝 <mail@ri-co.cn>
2021-04-26 09:15:22 +0800

    找不到路由报错级别调整

--- handlers/errors.go
+++ handlers/errors.go
@@ -7,7 +7,7 @@ import (
 
 //NotFound -handler 前端请求地址错误，调用此handler处理
 func NotFound(ctx iris.Context) {
-	log.Log.Errorln("404-找不到此路由/路径:", ctx.RequestPath(true))
+	log.Log.Warnln("404-找不到此路由/路径:", ctx.RequestPath(true))
 	ctx.StatusCode(404)
 	ctx.WriteString("路由/请求地址错误")
 }
